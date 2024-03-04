313a62d4476d1f9d64402e9ce01df1dd14825f15
Pasha <vasilievpavel795@gmail.com>
2024-03-05 01:40:16 +0300

    test11

--- server/api/notification/sendNotification/index.js
+++ server/api/notification/sendNotification/index.js
@@ -3,20 +3,20 @@ import webpush from "web-push";
 const vapidDetails = {
   publicKey: process.env.VAPID_PUBLIC_KEY,
   privateKey: process.env.VAPID_PRIVATE_KEY,
-  subject: process.env.VAPID_SUBJECT,
+  subject: "mailto:vasilievpavel795@gmail.com",
 };
 
 webpush.setVapidDetails(
-  "mailto:vasilievpavel795@gmail.com",
+  vapidDetails.subject,
   vapidDetails.publicKey,
   vapidDetails.privateKey
 );
 
 export default defineEventHandler(async (event) => {
   try {
+    const pushSubscription = JSON.parse(YOUR_PUSH_SUBSCRIPTION_STRING);
+    console.log(pushSubscription);
     const tempData = await readBody(event);
-    const pushSubscription = JSON.parse(tempData);
-
     const notification = JSON.stringify({
       title: "Hello, Notifications123!",
       options: {
@@ -28,7 +28,7 @@ export default defineEventHandler(async (event) => {
       vapidDetails: vapidDetails,
     };
     // Отправка уведомления
-    await webpush.sendNotification(pushSubscription, notification, options);
+    await webpush.sendNotification(tempData, notification, options);
     // console.log(subscription);
     console.log("success");
     return { success: true };
